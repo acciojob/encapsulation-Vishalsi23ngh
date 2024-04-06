10658f7cd210120bc95ecf6556e1981c9bb5df6b
Vishal Singh <vishalsingh23101999@gmail.com>
2024-04-06 22:39:46 +0530

    this is first change

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+        //obj.name = "Vishal"; // java: name has private access in com.driver.RWOnly
+        //System.out.println(obj.name);
+        //java: name has private access in com.driver.RWOnly
+
+        obj.setName("vishal");
+        System.out.println("Name : "+obj.getName());
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,19 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+
+    public RWOnly(){
+
+    }
+    public RWOnly(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
 }
